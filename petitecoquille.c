88e5459bf57aee749d1da31a049ef6807d7c732b
maxencegarcia <saeinfob1@gmail.com>
2026-09-26 00:41:51 +0200

    pipe

--- petitecoquille.c
+++ petitecoquille.c
@@ -10,6 +10,7 @@ int main() {
     char ch[100] = "";
     char opt[100] = "";
     char *argv[100];
+    char *argv2pipe[100];
     while ((strcmp(ch, "exit") != 0 )) {
         printf("$");
 
@@ -55,17 +56,41 @@ int main() {
         }
         int fichi = -2;
         int sortie = -2;
+        int erreurredirect = 0;
+        int pipeverif = 0;
+        int fd[2];
         for (i = 0; argv[i] != NULL; i++) {
             if (strcmp(argv[i], ">") == 0) {
                 i++;
                 fichi = open(argv[i], O_RDWR | O_CREAT | O_TRUNC , 0644);
+                if (fichi == -1) {
+                    perror("open");
+                    erreurredirect = -1 ;
+                }
                 i-=1;
                 argv[i] = NULL;
                 sortie = 1;
             }
+            else if (strcmp(argv[i], "|") == 0) {
+                int z = 0;
+                for (int j = i+1; argv[j] != NULL; j++) {
+                    argv2pipe[z] = argv[j];
+                    z++;
+                }
+                argv2pipe[z] = NULL;
+                argv[i] = NULL;
+                pipeverif = 1;
+
+            }
             else if (strcmp(argv[i], "<") == 0) {
                 i++;
                 fichi = open(argv[i], O_RDONLY);
+                if (fichi == -1) {
+                    perror("open");
+                    erreurredirect = -1 ;
+                }
+
+
                 i-=1;
                 argv[i] = NULL;
                 sortie = 0;
@@ -73,29 +98,76 @@ int main() {
             else if (strcmp(argv[i], ">>") == 0) {
                 i++;
                 fichi = open(argv[i], O_RDWR | O_CREAT | O_APPEND, 0644);
+                if (fichi == -1) {
+                    perror("open");
+                    erreurredirect = -1 ;
+                }
                 i-=1;
                 argv[i] = NULL;
                 sortie = 1;
             }
         }
+        if (erreurredirect== -1)
+            continue;
+        if (pipeverif == 1) {
+            if(pipe(fd)==-1) {
+                perror("pipe");
+            }else {
+                pid_t enfant1 = fork();
+                if (enfant1 == -1) {
+                    perror("fork gosse 1");
+                }
+                if (enfant1 == 0) {
+                    dup2(fd[1], STDOUT_FILENO);
+                    close(fd[0]);
+                    close(fd[1]);
+                    if (execvp(argv[0], argv) == -1) {
+                        perror("execvp");
+                        exit(1);
+                    }
+                }
+                pid_t enfant2 = fork();
+                if (enfant2 == -1) {
+                    perror("fork gosse 2");
+                }
+                if (enfant2 == 0) {
+                    dup2(fd[0], STDIN_FILENO);
+                    close(fd[1]);
+                    close(fd[0]);
+                    if (execvp(argv2pipe[0], argv2pipe) == -1) {
+                        perror("execvp");
+                        exit(1);
+                    }
+                }
+                if (enfant1 != -1 && enfant2 != -1) {
+                    close(fd[1]);
+                    close(fd[0]);
+                    waitpid(enfant1, NULL, 0);
+                    waitpid(enfant2, NULL, 0);
+                }
 
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("fork");
-        }
-        else if (pid == 0) {
-            if (fichi!= -2 && sortie != -2) {
-                dup2(fichi, sortie);
             }
 
-            if (execvp(argv[0], argv) == -1) {
-                perror("execvp");
-                exit(1);
-            }
-
         }
-        else {
-            wait(NULL);
+        if (pipeverif != 1) {
+            pid_t pid = fork();
+            if (pid == -1) {
+                perror("fork");
+            }
+            else if (pid == 0) {
+                if (fichi!= -2 && sortie != -2) {
+                    dup2(fichi, sortie);
+                }
+
+                if (execvp(argv[0], argv) == -1) {
+                    perror("execvp");
+                    exit(1);
+                }
+
+            }
+            else {
+                wait(NULL);
+            }
         }
 
     }
